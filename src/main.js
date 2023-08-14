68c6ef11f3e25c2c01a7f78062eb483d495bccdd
Bonifase <orwabonifase@gmail.com>
2023-08-14 10:45:00 +0300

    add package builder

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+const isMac = process.platform === 'darwin';
+
 // modify your existing createWindow() function
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
+  const mainWindow = new BrowserWindow({
+    title: "Electron Test App",
+    width: 500,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'src/preload.js')
     }
   })
 
-  win.loadFile('src/index.html')
+  mainWindow.loadFile('src/renderer/index.html')
 }
 
 app.whenReady().then(() => {
@@ -20,5 +23,10 @@ app.whenReady().then(() => {
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-})
+});
 
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
